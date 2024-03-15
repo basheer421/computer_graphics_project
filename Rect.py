8a818be3195fc60524181f05f5e4891e4f216968
basheer421 <basheer753@gmail.com>
2024-03-16 03:11:04 +0400

    Refactor Rect class to use consistent indentation

--- Rect.py
+++ Rect.py
@@ -2,49 +2,49 @@ from Shape import *
 
 
 class Rect(Shape):
-    def __init__(self, x, y, width, height,
-                 angle=0,
-                 borderColor=(1.0, 1.0, 1.0),
-                 fillColor=(0, 0, 0)):
+	def __init__(self, x, y, width, height,
+				 angle=0,
+				 borderColor=(1.0, 1.0, 1.0),
+				 fillColor=(0, 0, 0)):
 
-        super().__init__(x, y)
-        self.width = width
-        self.height = height
-        self.borderColor = borderColor
-        self.fillColor = fillColor
+		super().__init__(x, y)
+		self.width = width
+		self.height = height
+		self.borderColor = borderColor
+		self.fillColor = fillColor
 
-        # Calculate the center of the rectangle
-        self.center = (self.x + self.width / 2, self.y + self.height / 2)
+		# Calculate the center of the rectangle
+		self.center = (self.x + self.width / 2, self.y + self.height / 2)
 
-        # Initialize vertices
-        self.vertices = [
-            (self.x, self.y),
-            (self.x + self.width, self.y),
-            (self.x + self.width, self.y + self.height),
-            (self.x, self.y + self.height)
-        ]
+		# Initialize vertices
+		self.vertices = [
+			(self.x, self.y),
+			(self.x + self.width, self.y),
+			(self.x + self.width, self.y + self.height),
+			(self.x, self.y + self.height)
+		]
 
-        # Rotate the rectangle
-        self.rotate(angle)
+		# Rotate the rectangle
+		self.rotate(angle)
 
-    def draw(self):
-        glPushMatrix()
+	def draw(self):
+		glPushMatrix()
 
-        glTranslatef(self.center[0], self.center[1], 0)
-        glRotatef(self.angle, 0, 0, 1)
-        glTranslatef(-self.center[0], -self.center[1], 0)
+		glTranslatef(self.center[0], self.center[1], 0)
+		glRotatef(self.angle, 0, 0, 1)
+		glTranslatef(-self.center[0], -self.center[1], 0)
 
-        glColor3f(*self.fillColor)
-        glBegin(GL_QUADS)
-        for x, y in self.vertices:
-            glVertex2f(x, y)
-        glEnd()
+		glColor3f(*self.fillColor)
+		glBegin(GL_QUADS)
+		for x, y in self.vertices:
+			glVertex2f(x, y)
+		glEnd()
 
-        glColor3f(*self.borderColor)
-        glBegin(GL_LINES)
-        for i in range(4):
-            glVertex2f(*self.vertices[i])
-            glVertex2f(*self.vertices[(i + 1) % 4])
-        glEnd()
+		glColor3f(*self.borderColor)
+		glBegin(GL_LINES)
+		for i in range(4):
+			glVertex2f(*self.vertices[i])
+			glVertex2f(*self.vertices[(i + 1) % 4])
+		glEnd()
 
-        glPopMatrix()
+		glPopMatrix()
